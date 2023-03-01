cbb76bc6b2c9f0e685bcc2569be2a46460399064
michaelmensa <michaelmensa@live.com>
2023-03-01 15:15:30 +0000

    commit tasks 5 to 7

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# A regex that matches a string that starts with h and ends with n and can have any single character in between
+puts ARGV[0].scan(/h.n/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# A regex that matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
