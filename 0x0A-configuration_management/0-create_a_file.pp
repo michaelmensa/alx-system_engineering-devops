858cdfea6447cd07088b148bfca219cae34745a9
michaelmensa <michaelmensa@live.com>
2023-08-26 00:30:41 +0000

    task 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp
+
+file { 'school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+}
