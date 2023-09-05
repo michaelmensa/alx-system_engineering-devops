50ca1a00890a3a81c521cb7082886af9e8ab7007
michaelmensa <michaelmensa@live.com>
2023-09-05 10:43:42 +0000

    adv task update 1.1

--- 0x0F-load_balancer/2-custom_http_response_header.pp
+++ 0x0F-load_balancer/2-custom_http_response_header.pp
@@ -7,9 +7,9 @@ exec {'update':
   ensure => 'present',
 }
 -> file_line {'http_header':
-  path => '/etc/nginx/nginx.conf',
+  path  => '/etc/nginx/nginx.conf',
   match => 'http {',
-  line => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 -> exec {'run':
   command => '/usr/sbin/service nginx restart',
