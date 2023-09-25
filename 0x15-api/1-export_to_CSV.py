243fc3758012aaf7efd4f5154724e3e123d49778
michaelmensa <michaelmensa@live.com>
2023-09-25 15:49:58 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Accessing a REST API for todo list of employees '''
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    empID = sys.argv[1]
+    baseUrl = 'https://jsonplaceholder.typicode.com/users'
+    url = baseUrl + '/' + empID
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + '/todos'
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    csv_file = f'{empID}.csv'
+    with open(csv_file, 'w') as file:
+        for task in tasks:
+            file.write(f'"{empID}", "{username}",\
+                       "{task.get("completed")}", "{task.get("title")}"\n')
