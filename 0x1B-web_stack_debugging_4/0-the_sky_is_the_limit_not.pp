32a131d361104c946043976a207f21c7bc6178b8
michaelmensa <michaelmensa@live.com>
2023-10-19 20:20:10 +0000

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the nginx config file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart nginx
+exec { 'restart-nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/local/bin/', '/bin/', '/usr/bin/'],
+}
